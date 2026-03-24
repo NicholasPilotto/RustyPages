c62850df6296444394ff0cb873511e712601158f
NicholasPilotto <nicholaspilotto@gmail.com>
2026-03-24 22:32:46 +0100

    Correct Book entity declaratin

--- src/domain/book.rs
+++ src/domain/book.rs
@@ -1,8 +1,31 @@
-use serde::{Serialize, Deserialize};
+use sea_orm_migration::seaql_migrations::Relation;
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
-use uuid::Uuid;
 
-#[derive(Serialize, Deserialize, ToSchema)]
-pub struct Book {
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Serialize, Deserialize, ToSchema)]
+#[sea_orm(table_name = "books")]
+pub struct Model {
+    #[sea_orm(primary_key, auto_increment = false)]
     pub id: Uuid,
+
+    pub title: String,
+    pub description: Option<String>,
+
+    pub author: String,
+    pub isbn: Option<String>,
+
+    #[schema(value_type = String, format = "date-time", nullable = true)]
+    pub published_at: Option<DateTimeUtc>,
+
+    pub pages: Option<i32>,
+    pub language: Option<String>,
+
+    #[schema(value_type = String, format = "date-time")]
+    pub created_at: DateTimeUtc,
+
+    #[schema(value_type = String, format = "date-time")]
+    pub updated_at: DateTimeUtc,
 }
+
+impl ActiveModelBehavior for ActiveModel {}
